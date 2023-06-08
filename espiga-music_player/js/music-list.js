65ba1ae100d3f16d932d8036d4f49e001ceb9e53
Liezel Joy <liezeljoy.espiga@zdspgc.edu.ph>
2023-06-08 20:02:07 +0800

    espiga-music player

---
+++ espiga-music_player/js/music-list.js
@@ -0,0 +1,182 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+        name: "After All this Year",
+    artist: "Journey",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+        name: "After All this Year",
+    artist: "Journey",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Ever Since The World Began",
+    artist: "Survivor",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Wonderful Tonight",
+    artist: "Eric Clapton",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: " A thousand Years",
+    artist: "Christine Perri",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Beautiful in White",
+    artist: "Westlife",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Against All Odds",
+    artist: "Mariah Carey ",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Desert",
+    artist: "Dawin",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+        name: "Huling El Bimbo",
+    artist: "Eraserheads",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "When I was Your Man",
+    artist: "Bruno Mars ",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Over You",
+    artist: "Chris Daughtry",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+        name: "The Past",
+    artist: "Jed Madela",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "This I Promise You ",
+    artist: "N'sync ",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Speak Now",
+    artist: "TAylor Swift",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+        name: "Heaven Knows",
+    artist: "Rick Price",
+    img: "music-20",
+    src: "music-20"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
